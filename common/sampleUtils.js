914377ff3d39bcd0f3036f995986ca409ba11081
cyclonezhao <lightseeker@qq.com>
2018-10-12 15:13:04 +0800

    修改采样逻辑

--- common/sampleUtils.js
+++ common/sampleUtils.js
@@ -3,16 +3,15 @@ define(function (){
 		// 采样
 		gatherSamples: function(sampleRange, sampleCount){
 			var sampleSet = [];
-			var unitSample = sampleRange;
-			if(sampleCount > 1){
-				unitSample = sampleRange / (sampleCount - 1)
-			}
-			for(var i = 0; i < sampleCount; i++){
-				sampleSet.push(unitSample * i);
+			sampleSet.push(0);
+
+			if(sampleCount > 0){
+				var unitSample = sampleRange / (sampleCount + 1);
+				for(var i = 1; i <= sampleCount; i++){
+					sampleSet.push(unitSample * i);
+				}
 			}
 
-			// 消除精度问题，最后一个样品值强制等于采样区间的上界
-			var last = sampleSet.pop();
 			sampleSet.push(sampleRange);
 			return sampleSet;
 		}
--- test/common/testSampleUtils.js
+++ test/common/testSampleUtils.js
@@ -1,20 +1,12 @@
 define(["../../common/sampleUtils"], function (sampleUtils) {
 	return{
 		test: function(assert){
-			// 3, 1 -> [0,3]
+			// 3, 1 -> [0, 3/2, 3]
 			var ret = sampleUtils.gatherSamples(3, 1);
-			assert.equal(ret.length, 1, "输入3, 1， 输出样品的数量");
-			assert.equal(ret[0], 3, "输入3, 1， 输出末项");
-			// 3, 2 -> [0, ?, 3]
-			ret = sampleUtils.gatherSamples(3, 2);
-			assert.equal(ret.length, 2, "输入3, 2， 输出样品的数量");
-			assert.equal(ret[0], 0, "输入3, 2， 输出首项");
-			assert.equal(ret[1], 3, "输入3, 2， 输出末项");
-			// 4, 3 -> [0, ?, ?, 4]
-			ret = sampleUtils.gatherSamples(4, 3);
-			assert.equal(ret.length, 3, "输入4, 3， 输出样品的数量");
-			assert.equal(ret[0], 0, "输入4, 3， 输出首项");
-			assert.equal(ret[2], 4, "输入4, 3， 输出末项");
+			assert.equal(ret.length, 3, "输入3, 1， 输出样品的数量");
+			assert.equal(ret[0], 0, "输入3, 1， 输出首项");
+			assert.equal(ret[1], 3/2, "输入3, 1， 输出中项");
+			assert.equal(ret[2], 3, "输入3, 1， 输出末项");
 		}
 	};
 });
